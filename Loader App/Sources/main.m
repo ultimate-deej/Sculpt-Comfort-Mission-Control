aaaacb5d8d58803915bb5a06e79347d4b4d90199
Maxim Naumov <ultimate.deej@gmail.com>
2018-01-07 21:52:28 +0300

    Loader App: trigger scripting addition

--- Loader App/Sources/main.m
+++ Loader App/Sources/main.m
@@ -6,6 +6,16 @@
 //  Copyright © 2018 deej. All rights reserved.
 //
 
+@import Foundation;
+
 int main(int argc, const char * argv[]) {
-    return 0;
+    NSAppleScript *triggerOsaxScript = [[NSAppleScript alloc] initWithSource:@"tell application \"Dock\" to «event SCMCinjt»"];
+    NSDictionary<NSString *, id> *error = nil;
+    [triggerOsaxScript executeAndReturnError:&error];
+    if (error != nil) {
+        // TODO: show alert
+        return 1;
+    } else {
+        return 0;
+    }
 }
