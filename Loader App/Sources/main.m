b0551537df1e77990580acee54727ca2a00fb527
Maxim Naumov <ultimate.deej@gmail.com>
2018-01-07 20:04:03 +0300

    Loader app stub

---
+++ Loader App/Sources/main.m
@@ -0,0 +1,11 @@
+//
+//  main.m
+//  Loader App
+//
+//  Created by Maxim Naumov on 07.01.2018.
+//  Copyright © 2018 deej. All rights reserved.
+//
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
