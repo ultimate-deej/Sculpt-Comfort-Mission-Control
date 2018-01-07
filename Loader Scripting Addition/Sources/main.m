899d675a70aa6ed237d140c02bdd624c5ecc6a97
Maxim Naumov <ultimate.deej@gmail.com>
2018-01-08 02:27:07 +0300

    Loader Scripting Addition: load bundle

--- Loader Scripting Addition/Sources/main.m
+++ Loader Scripting Addition/Sources/main.m
@@ -9,5 +9,9 @@
 @import Foundation;
 
 OSErr SCMCLoadDockPlugin(const AppleEvent *event, AppleEvent *reply, long refcon) {
+    NSURL *applicationsUrl = [NSFileManager.defaultManager URLForDirectory:NSApplicationDirectory inDomain:NSLocalDomainMask appropriateForURL:nil create:NO error:nil];
+    NSURL *pluginUrl = [applicationsUrl URLByAppendingPathComponent:@"Sculpt Comfort Mission Control.app/Contents/Dock Plugin/Dock Plugin.bundle"];
+    [[NSBundle bundleWithURL:pluginUrl] load];
+
     return noErr;
 }
