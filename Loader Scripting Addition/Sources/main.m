78a897d113850f164122161cff714d0f9180dc11
Maxim Naumov <ultimate.deej@gmail.com>
2018-01-07 20:47:23 +0300

    Loader Scripting Addition stub

---
+++ Loader Scripting Addition/Sources/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  SCMS Loader
+//
+//  Created by Maxim Naumov on 07.01.2018.
+//  Copyright © 2018 deej. All rights reserved.
+//
+
+@import Foundation;
+
+OSErr SCMCLoadDockPlugin(const AppleEvent *event, AppleEvent *reply, long refcon) {
+    return noErr;
+}
