2fea08e933f1f7e46199ba7e6fa251d0f1ee3206
Maxim Naumov <ultimate.deej@gmail.com>
2018-01-09 04:59:17 +0300

    Shared project: add another target which will have different class names (via macros)
    Preference Pane requires classes to have long prefixes to prevent name conflicts

---
+++ Shared/Sources/PrefixedNames.h
@@ -0,0 +1,12 @@
+//
+//  PrefixedNames.h
+//  SharedPrefixedNames
+//
+//  Created by Maxim Naumov on 09.01.2018.
+//  Copyright © 2018 deej. All rights reserved.
+//
+
+#ifdef PREFIX_NAMES_FOR_PREFERENCE_PANE
+
+
+#endif
