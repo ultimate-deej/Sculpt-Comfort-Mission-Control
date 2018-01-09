4658ab14d593e92b5249a5ea7e3cf18298d5e4b2
Maxim Naumov <ultimate.deej@gmail.com>
2018-01-09 03:49:20 +0300

    Preference Pane stub

---
+++ Preference Pane/Sources/PreferencePane.h
@@ -0,0 +1,13 @@
+//
+//  PreferencePane.h
+//  Preference Pane
+//
+//  Created by Maxim Naumov on 09.01.2018.
+//  Copyright © 2018 deej. All rights reserved.
+//
+
+@import PreferencePanes;
+
+@interface DeejScmcPreferencePane : NSPreferencePane
+
+@end
---
+++ Preference Pane/Sources/PreferencePane.m
@@ -0,0 +1,13 @@
+//
+//  PreferencePane.m
+//  Preference Pane
+//
+//  Created by Maxim Naumov on 09.01.2018.
+//  Copyright © 2018 deej. All rights reserved.
+//
+
+#import "PreferencePane.h"
+
+@implementation DeejScmcPreferencePane
+
+@end
