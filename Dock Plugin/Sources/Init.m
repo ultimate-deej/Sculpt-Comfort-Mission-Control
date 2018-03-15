fc5e91f2d810b5c0fb9c053399c25abb7f35a513
Maxim Naumov <ultimate.deej@gmail.com>
2018-03-15 22:00:26 +0300

    Initialization stub

---
+++ Dock Plugin/Sources/Init.m
@@ -0,0 +1,13 @@
+//
+//  Init.m
+//  Dock Plugin
+//
+//  Created by Maxim Naumov on 15.03.2018.
+//  Copyright © 2018 deej. All rights reserved.
+//
+
+// MARK: - Initializing
+
+__attribute__((constructor))
+static void StartCore(void) {
+}
